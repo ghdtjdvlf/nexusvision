bfdfa60e7d958d587b86bb3b50acdd17a16eba99
ghdtjdvlf <tjdvlf0416@gmail.com>
2025-05-12 02:18:31 +0900

    2

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,10 +1,17 @@
+<<<<<<< Updated upstream
   const container = document.querySelector('.s_container')
   
   var swiper = new Swiper(".mySwiper", {
+=======
+const container = document.querySelector(".swiper_container")    
+
+    var swiper = new Swiper(".mySwiper", {
+>>>>>>> Stashed changes
       direction: "vertical",
       slidesPerView: 3.5, 
       speed: 2400,
       loop:true,
+<<<<<<< Updated upstream
       freeMode: true,
       allowTouchMove: false,
       pagination: {
@@ -28,14 +35,41 @@ function startAutoplay() {
     container.addEventListener('mouseenter',() => stopAutoplay())
     container.addEventListener('mouseleave',() => startAutoplay())
 
-
-    var logoSwiper = new Swiper(".logoSwiper", {
-      slidesPerView: 4, 
-      speed: 3000,
-      loop:true,
-      allowTouchMove: false,
-      autoplay: {
-        delay: 0,
-        pauseOnMouseEnter : false
-    },
+=======
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      }, 
+      autoplay : {
+        delay : 0
+      }
     });
+
+    const stopAutoplay = () => {
+      const swiperTranslate = swiper.getTranslate();
+      swiper.setTranslate(swiperTranslate);
+      swiper.autoplay.stop();
+    }
+    const startAutoplay = () => {
+      swiper.slideTo(swiper.activeIndex, 3000, false);
+      swiper.autoplay.start();
+    }
+  
+  
+    container.addEventListener("mouseenter", () => stopAutoplay())
+    container.addEventListener("mouseleave", () => startAutoplay())
+
+    
+  
+>>>>>>> Stashed changes
+
+    // var logoSwiper = new Swiper(".logoSwiper", {
+    //   slidesPerView: 4, 
+    //   speed: 3000,
+    //   loop:true,
+    //   allowTouchMove: false,
+    //   autoplay: {
+    //     delay: 0,
+    //     pauseOnMouseEnter : false
+    // },
+    // });
