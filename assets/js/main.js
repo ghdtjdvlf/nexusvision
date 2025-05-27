50e730ef29cd91ab0a1beb7fd3da6dd9792ea2bf
ghdtjdvlf <tjdvlf0416@gmail.com>
2025-05-27 18:28:12 +0900

    content page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+//header 
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const toggleBtn = document.querySelector('.toggle-btn');
+            const mobileMenu = document.querySelector('.mobile-menu');
+            const overlay = document.querySelector('.overlay');
+            
+            toggleBtn.addEventListener('click', function() {
+                mobileMenu.classList.toggle('active');
+                overlay.classList.toggle('active');
+            });
+            
+            overlay.addEventListener('click', function() {
+                mobileMenu.classList.remove('active');
+                overlay.classList.remove('active');
+            });
+        });
+
+
 //KV 커지는 영상
 
  document.addEventListener('DOMContentLoaded', () => {
