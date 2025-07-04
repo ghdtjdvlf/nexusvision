7e8b97e79d59cab9f244a697e536050453e01c34
ghdtjdvlf <tjdvlf0416@gmail.com>
2025-07-04 15:27:29 +0900

    오류 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,68 +16,7 @@ window.addEventListener('scroll', () => {
 
  
 
-//sec05 text 
-document.addEventListener('DOMContentLoaded', function() {
-  const text1 = document.getElementById('text1');
-  const text2 = document.getElementById('text2');
-  const text3 = document.getElementById('text3');
-  const progressBar = document.querySelector('.progress-bar');
 
-  function isMobile() {
-    return window.innerWidth <= 480;
-  }
-  function isTablet() {
-    return window.innerWidth > 480 && window.innerWidth <= 768;
-  }
-  function isLaptop() {
-    return window.innerWidth > 768 && window.innerWidth <= 1280;
-  }
-
-  window.addEventListener('scroll', function() {
-      const scrollY = window.scrollY || window.pageYOffset;
-
-      let scale = 1; // 기본 scale
-
-      if (isMobile()) {
-          scale = 0.6;
-      } else if (isTablet()) {
-          scale = 0.4;
-      } else if (isLaptop()) {
-          scale = 0.9;
-      }
-
-      const thresh1 = 4900 * scale;
-      const thresh2 = 5900 * scale;
-      const thresh3 = 6900 * scale;
-      const threshHide = 7500 * scale;
-
-      if (scrollY >= threshHide) {
-          text1.classList.remove('active');
-          text2.classList.remove('active');
-          text3.classList.remove('active');
-      } 
-      else if (scrollY >= thresh3) {
-          text1.classList.remove('active');
-          text2.classList.remove('active');
-          text3.classList.add('active');
-      } 
-      else if (scrollY >= thresh2) {
-          text1.classList.remove('active');
-          text2.classList.add('active');
-          text3.classList.remove('active');
-      } 
-      else if (scrollY >= thresh1) {
-          text1.classList.add('active');
-          text2.classList.remove('active');
-          text3.classList.remove('active');
-      } 
-      else {
-          text1.classList.remove('active');
-          text2.classList.remove('active');
-          text3.classList.remove('active');
-      }
-  });
-});
 
 
 
